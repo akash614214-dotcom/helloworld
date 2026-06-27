a1ac6387e0a22e09a37dbf4a0c30fc8d1944e61d
Akash Sharma <akash614214@gmail.com>
2026-06-27 12:26:06 +0530

    squre1

---
+++ squre.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+    int a, squre;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+    squre = a*a;
+    printf("The square of %d is %d ", a, squre);
+    return 0;
+}
